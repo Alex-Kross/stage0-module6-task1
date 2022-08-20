d79faa7ad01f11a2fc293142e7ca51fffd5cead7
Alex-Kross <artemkirlanovia@gmail.com>
2022-08-20 20:18:56 +0300

    task finished

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,35 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        StringBuilder description = new StringBuilder();
+
+        description.append("This animal is mostly ");
+        description.append(color);
+        description.append(". It has ");
+        description.append(numberOfPaws);
+
+        if (numberOfPaws > 1) {
+            description.append(" paws and ");
+        } else {
+            description.append(" paw and ");
+        }
+        if (hasFur) {
+            description.append("a fur.");
+        } else {
+            description.append("no fur.");
+        }
+
+        return description.toString();
+    }
 }
